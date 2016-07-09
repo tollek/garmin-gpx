c70da4e70ba8b331e4cb6641b10e90d17da30fbb
Paweł Borkowski <pawel.borkowski@zpas-lab.pl>
2016-07-09 12:58:44 +0200

    Fix header extraction in split_gpx.py

--- split_gpx.py
+++ split_gpx.py
@@ -4,16 +4,22 @@ import os
 from bs4 import BeautifulSoup
 
 GPX_FILE = 'garmin.gpx'
-GPX_HEADER_LINES = 30
 OUTPUT_DIR = 'garmin_data'
 
 def split_gpx():
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
+  
+    # Find .gpx header - all lines until the first 'trk'
+    header = open(GPX_FILE, 'r').readlines()
+    for idx, header_line in enumerate(header):
+        if 'trk' in header_line:
+            header = header[:idx]
+            break            
 
-    header = open(GPX_FILE, 'r').readlines()[:GPX_HEADER_LINES]
+    # Build single .gpx file for every 'trk' section.
     with open(GPX_FILE, 'r') as gpx_file:
-        soup = BeautifulSoup(gpx_file)
+        soup = BeautifulSoup(gpx_file, 'lxml')
         trks = soup.findAll('trk')
         for trk in trks:
             start_time = trk.find('time').string.replace(':', '_')
