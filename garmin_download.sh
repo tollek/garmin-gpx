7e9032ad2c0196094b50bda9ab4baa6f19fc71d5
Paweł Borkowski <pawel.borkowski@zpas-lab.pl>
2016-07-09 12:06:55 +0200

    Run gpslabel as sudo (fixes 'could not claim interface 0: Operation not permitted')

--- garmin_download.sh
+++ garmin_download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mv garmin.gpx garmin.gpx.backup
-gpsbabel -w -r -t -i garmin,snwhite=0,get_posn=0,power_off=0,erase_t=0,resettime=0 -f usb: -o gpx,suppresswhite=0,logpoint=0,humminbirdextensions=0,garminextensions=0 -F garmin.gpx
+sudo gpsbabel -w -r -t -i garmin,snwhite=0,get_posn=0,power_off=0,erase_t=0,resettime=0 -f usb: -o gpx,suppresswhite=0,logpoint=0,humminbirdextensions=0,garminextensions=0 -F garmin.gpx
 mkdir garmin_data
 
